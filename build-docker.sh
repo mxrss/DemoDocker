2baacfc404955fb2fb1bb2c620dde8b4e46f0811
Mike Roth <mike.roth@mxrss.com>
2015-10-28 19:57:10 -0700

    Update to build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,19 @@
+#/var/bin/sh
+DOCKERMACHINE_STATUS=$(docker-machine ls | grep "default")
+
+trim() {
+    local var="$*"
+    var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
+    echo "$var"
+}
+
+if [$(trim $DOCKERMACHINE_STATUS) -eq '']; then
+  echo "Creating docker machine 'default'"
+  $(docker-machine create --driver generic --generic-ip-address localhost "default")
+  echo "Docker machine connection 'default' created"
+fi
+
+echo "Setting Docker machine env!"
+eval "$(docker-machine env default)"
+docker-compose up
